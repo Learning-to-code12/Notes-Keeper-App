a6a7489dd7819b7128cc11efcf2055e719aa9af2
Learning-to-code12 <66002514+Learning-to-code12@users.noreply.github.com>
2020-12-12 16:45:34 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+
+
+function App(){
+  return(
+      <div>
+          <Header />
+          {notes.map(function createNotes(noteItem){
+              return (<Note
+                  key={noteItem.key}
+                  title={noteItem.title}
+                  content={noteItem.content}
+              />);
+            })};
+          <Footer />
+      </div>
+  );
+}
+
+export default App;
