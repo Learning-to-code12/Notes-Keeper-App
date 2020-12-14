aa5e371b55f3d619a7598fb27349eaf49469ecdd
Learning-to-code12 <66002514+Learning-to-code12@users.noreply.github.com>
2020-12-14 14:01:19 +0530

    Update App.jsx
    Added more functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
+function App() {
+  const [notes, setNotes] = useState([]);
 
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-function App(){
-  return(
-      <div>
-          <Header />
-          {notes.map(function createNotes(noteItem){
-              return (<Note
-                  key={noteItem.key}
-                  title={noteItem.title}
-                  content={noteItem.content}
-              />);
-            })};
-          <Footer />
-      </div>
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
   );
 }
 
